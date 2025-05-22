611fc52f35926166a66caaead6977570c41b3f69
jonathan.ferreyra <jonathan.ferreyra@invgate.com>
2025-05-22 18:31:37 -0300

    get issue: allow to indicate list of includes

--- nodes/Redmine/IssueOperations.ts
+++ nodes/Redmine/IssueOperations.ts
@@ -65,6 +65,56 @@ export const issueFields: INodeProperties[] = [
     default: '',
     description: 'The ID of the issue to get',
   },
+  {
+    displayName: 'Include',
+    name: 'include',
+    type: 'multiOptions',
+    displayOptions: {
+      show: {
+        resource: ['issue'],
+        operation: ['get'],
+      },
+    },
+    options: [
+      {
+        name: 'Attachments',
+        value: 'attachments',
+        description: 'Include attachments (Since Redmine 3.4.0)',
+      },
+      {
+        name: 'Allowed statuses',
+        value: 'allowed_statuses',
+        description: 'Include the available allowed statuses (Since Redmine 5.0.x)',
+      },
+      {
+        name: 'Changesets',
+        value: 'changesets',
+        description: 'Include issue changesets',
+      },
+      {
+        name: 'Children',
+        value: 'children',
+        description: 'Include child issues',
+      },
+      {
+        name: 'Journals',
+        value: 'journals',
+        description: 'Include issue history',
+      },
+      {
+        name: 'Relations',
+        value: 'relations',
+        description: 'Include related issues',
+      },
+      {
+        name: 'Watchers',
+        value: 'watchers',
+        description: 'Include watchers (Since Redmine 2.3.0)',
+      }
+    ],
+    default: [],
+    description: 'Associated data to include in the response',
+  },
   // Fields for GET ALL operation
   ...issueGetAllOperations,
 
